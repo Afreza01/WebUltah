6f990c17e1c133a4307b3568417856ba8614f449
Afreza Arya Putra <afrezkerja01@gmail.com>
2025-05-31 16:53:42 +0700

    1

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    setTimeout(() => {
+      document.getElementById("preloader").style.display = "none";
+      document.getElementById("main-content").style.display = "block";
+    }, 3000); // delay 2 detik
+  });
+  
