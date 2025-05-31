a0f291b3a9e6865e3767b85af1e175c721cb9977
Afreza Arya Putra <afrezkerja01@gmail.com>
2025-05-31 21:54:44 +0700

    update slide, index

--- main.js
+++ main.js
@@ -2,6 +2,24 @@ window.addEventListener("load", function () {
     setTimeout(() => {
       document.getElementById("preloader").style.display = "none";
       document.getElementById("main-content").style.display = "block";
-    }, 3000); // delay 2 detik
+    }, 2000); // delay 2 detik
   });
   
+  const text = "HAPPY BIRTHDAY";
+const typingText = document.getElementById("typing-text");
+const button = document.querySelector(".button");
+
+let index = 0;
+
+function type() {
+  if (index < text.length) {
+    typingText.textContent += text[index];
+    index++;
+    setTimeout(type, 500); // delay per huruf 150ms
+  } else {
+    // setelah selesai ketik, munculkan tombol
+    button.style.opacity = 1;
+  }
+}
+
+type();
